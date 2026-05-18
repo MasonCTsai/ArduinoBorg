20183a9de190f216e7b88efd11e34b495b6edbf9
Mason Tsai <mason.c.tsai@gmail.com>
2026-05-18 12:08:18 -0700

    update input

--- src/input.py
+++ src/input.py
@@ -10,10 +10,18 @@ port = serial.Serial(sys.argv[1], int(sys.argv[2]))
 
 while True:
     if keyboard.is_pressed('q'): break
-    x, y = 5, 5
-    if keyboard.is_pressed('w'): y += 1
-    if keyboard.is_pressed('a'): x -= 1
-    if keyboard.is_pressed('s'): y -= 1
-    if keyboard.is_pressed('d'): x += 1
-    output = "</w" + str(x) + str(y) + ">"
+    output = ""
+    if not keyboard.is_pressed('c'):
+        x, y = 5, 5
+        if keyboard.is_pressed('w'): y += 1
+        if keyboard.is_pressed('a'): x -= 1
+        if keyboard.is_pressed('s'): y -= 1
+        if keyboard.is_pressed('d'): x += 1
+        output = "</wm" + str(x) + str(y) + ">"
+    else:
+        x = 1
+        if keyboard.is_pressed('w'): x += 1
+        if keyboard.is_pressed('d'): x += 1
+        output = "</cm" + str(x) + ">"
+
     port.write(output.encode())
